02de65ebce1633918209ef8d3ba85267be8e7f75
Adel Valiullin <happosai10@gmail.com>
2021-11-28 09:20:11 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        int ticketPrice = 988823459;
+        int bonusMile = ticketPrice / 20;
+        System.out.println("Bonus mile = " + bonusMile);
+        System.out.println("Ticket = " + ticketPrice);
+        /*int testSelf = bonusMile * 20;*/
+        /*System.out.println("Ticket = " + testSelf);*/
+    }
+}
